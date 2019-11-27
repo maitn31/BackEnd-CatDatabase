24e5fc3d71e22630ae82b06659e36417e87131a5
Ren <crycatdo@gmail.com>
2019-11-27 14:13:50 +0200

    Week 5

--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,63 @@
 'use strict';
-
 const pool = require('../database/db');
 const promisePool = pool.promise();
 
 const getAllUsers = async () => {
   try {
-    const [rows] = await promisePool.query('SELECT * FROM wop_user');
+    const [rows] = await promisePool.execute('SELECT * FROM wop_user');
     return rows;
   } catch (e) {
     console.log('error', e.message);
+    return {error: 'error in database query'};
   }
 };
 
 const getUser = async (params) => {
   try {
     const [rows] = await promisePool.execute(
-        'SELECT * FROM wop_user WHERE user_id= ?;',
-        params
+        'SELECT * FROM wop_user WHERE user_id = ?;',
+        params,
     );
     return rows;
   } catch (e) {
     console.log('error', e.message);
+    return {error: 'error in database query'};
   }
 };
-
 
 const addUser = async (params) => {
   try {
     const [rows] = await promisePool.execute(
-        'INSERT INTO wop_user (name, email,password) VALUES (?, ?, ?);'      ,
-        params
+        'INSERT INTO wop_user (name, email, password) VALUES (?, ?, ?);',
+        params,
     );
     return rows;
   } catch (e) {
+    console.log('error', e.message);
+    return {error: 'error in database query'};
+  }
+};
+
+const updateUser = async (params) => {
+  try {
+    const [rows] = await promisePool.execute(
+        'UPDATE wop_user SET name = ?, password = ? WHERE user_id = ?;',
+        params);
+    return rows;
+  }
+  catch (e) {
+    console.log('error', e.message);
+  }
+};
+
+const deleteUser = async (params) => {
+  try {
+    const [rows] = await promisePool.execute(
+        'DELETE FROM wop_user WHERE cat_id = ?;',
+        params);
+    return rows;
+  }
+  catch (e) {
     console.log('error', e.message);
   }
 };
@@ -41,4 +66,6 @@ module.exports = {
   getAllUsers,
   getUser,
   addUser,
+  deleteUser,
+  updateUser,
 };
